61946c31ba2566eb2aff3ffd79fda160a56f6fe6
gondrac <gongarc10@gmail.com>
2025-06-12 18:38:47 -0500

    cambios de nombre

--- routes/gastos.js
+++ routes/gastos.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
-const Expense = require("../models/Expense");
+const Expense = require("../models/Gastos");
 require("dotenv").config();
 
 // Middleware de JWT
